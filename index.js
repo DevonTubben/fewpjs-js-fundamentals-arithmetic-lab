395a1653ec83ad47604a25c0dd2290ba2bf00342
Devon Tubben <tubbend1@msu.edu>
2021-04-12 11:44:00 -0400

    commit tree 5221495eac22ed4e1b65debd726dbd1956307466

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// multiply = (num1, num2) { 
+ //   return num1 * num2 
+// }
+
+const num1 = 2 
+const num2 = 31 
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() + Math.floor(1))
+console.log(random);
+
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4; 
+console.log(mod);
+
+const max = Math.max(20); 
+console.log(max);
